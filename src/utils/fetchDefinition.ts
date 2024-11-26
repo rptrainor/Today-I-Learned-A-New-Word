5987a1f8bb4d4c18f215540101dbc13609193849
Ryan Trainor <rptrainor@gmail.com>
2024-11-26 13:51:46 -0300

    add auth the api call

--- src/utils/fetchDefinition.ts
+++ src/utils/fetchDefinition.ts
@@ -11,6 +11,12 @@ export async function fetchDefinition(word: string) {
 	try {
 		const response = await fetch(
 			`https://api.wikimedia.org/core/v1/wiktionary/en/page/${word}/html`,
+			{
+				headers: {
+					Authorization: `Bearer ${import.meta.env.PUBLIC_WIKIPEDIA_ACCESS_TOKEN}`,
+					"Api-User-Agent": `${import.meta.env.PUBLIC_WIKIPEDIA_APP_NAME}`,
+				},
+			},
 		);
 		console.log({response});
 		if (!response.ok) {
